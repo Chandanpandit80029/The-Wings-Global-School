c4e69a16892bf3bd3b97c437b008be87a9abeb72
Chandan Pandit <125503076+Chandanpandit80029@users.noreply.github.com>
2026-05-20 23:32:46 +0530

    Change Router from BrowserRouter to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 
 // Detect deployment environment and set basename accordingly
 // GitHub Pages: /repository-name/
